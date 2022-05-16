c44f02d49ca560f79edc4fc287f4e4f9e4f40cfc
Ryan Haver <rmh7596@rit.edu>
2022-05-15 23:36:24 -0400

    Increased dataset and slight work on CNN

--- CNN.py
+++ CNN.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import os
 from PIL import Image
+import numpy as np
 import random
 import torch
 import torchvision.transforms as transforms
@@ -30,13 +31,14 @@ class Dataset(Dataset):
         # Shuffle the order of the images
         # Using a 80/20 split
         if train:
-            self.all_files = self.all_files[::1830]
-            self.labels = self.labels[::1830]
+            self.all_files = self.all_files[0:8335]
+            self.labels = self.labels[0:8335]
             self.len = len(self.all_files)
         else: 
-            self.all_files = self.all_files[1830::]
-            self.labels = self.labels[1830::]
+            self.all_files = self.all_files[8335::]
+            self.labels = self.labels[8335::]
             self.len = len(self.all_files)
+
         
     def __len__(self):
         return self.len
@@ -55,12 +57,12 @@ class CNN(nn.Module):
     def __init__(self, out_1, out_2, out_3):
         super(CNN, self).__init__()
         self.cnn1 = nn.Conv2d(in_channels=1, out_channels=out_1, kernel_size=5, padding=2) # Convolution layer, output is 160
-        self.maxpool1=nn.MaxPool2d(kernel_size=4) # Max pooling layer, output is 40
-        self.cnn2 = nn.Conv2d(in_channels=out_1, out_channels=out_2, kernel_size=5, padding=2) # Output is 40
-        self.maxpool2 = nn.MaxPool2d(kernel_size=2) # Output is 20
-        self.cnn3 = nn.Conv2d(in_channels=out_2, out_channels=out_3, kernel_size=5, padding=2) # Output is 20
-        self.maxpool3 = nn.MaxPool2d(kernel_size=2) # Output is 10
-        self.fc1 = nn.Linear(10*10*out_3, 26) # Fully connected neural network
+        self.maxpool1=nn.MaxPool2d(kernel_size=2) # Max pooling layer, output is 80
+        self.cnn2 = nn.Conv2d(in_channels=out_1, out_channels=out_2, kernel_size=5, padding=2) # Output is 80
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2) # Output is 40
+        #self.cnn3 = nn.Conv2d(in_channels=out_2, out_channels=out_3, kernel_size=5, padding=2) # Output is 20
+        #self.maxpool3 = nn.MaxPool2d(kernel_size=2) # Output is 10
+        self.fc1 = nn.Linear(40*40*out_2, 26) # Fully connected neural network
 
     def forward(self,x):
         x = self.cnn1(x)        # Convolution
@@ -71,9 +73,9 @@ class CNN(nn.Module):
         x = torch.relu(x)       # Activation
         x = self.maxpool2(x)    # Pooling
 
-        x = self.cnn3(x)        # Convolution
-        x = torch.relu(x)       # Activation
-        x = self.maxpool3(x)    # Pooling
+        #x = self.cnn3(x)        # Convolution
+        #x = torch.relu(x)       # Activation
+        #x = self.maxpool3(x)    # Pooling
 
         x = x.view(x.size(0), -1) # 1-D input
         x = self.fc1(x)
@@ -81,7 +83,7 @@ class CNN(nn.Module):
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = CNN(out_1=160, out_2=320, out_3=480)
+model = CNN(out_1=160, out_2=80, out_3=480)
 model.to(device)
 
 train_dataset = Dataset(transform=transforms.ToTensor(), train=True)
@@ -99,19 +101,23 @@ composed = transforms.Compose([transforms.ToTensor(), transforms.Resize([160,160
 train_dataset = Dataset(transform=composed, train=True)
 validation_dataset = Dataset(transform=composed, train=False)
 
-batch_size = 104
+batch_size = 52
 momentum = 0.9
-lr=0.0001
+lr=0.000001
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum)
 
 trainloader = DataLoader(dataset=train_dataset, batch_size=batch_size)
-validationloader = DataLoader(dataset=validation_dataset, batch_size=26)
+validationloader = DataLoader(dataset=validation_dataset, batch_size=batch_size)
 n_test = len(validation_dataset)
 
 loss_list = []
 accuracy_list = []
+test_image = []
+predicted_label = []
+actual_label = []
+sample_number = []
 
 def train_model(n_epochs):
     correct = 0
@@ -134,30 +140,46 @@ def train_model(n_epochs):
             _, yhat = torch.max(z.data, 1)
             correct += (yhat == y_test).sum().item()
 
+            for i in range(len(x_test)):
+                if not (yhat[i]==y_test[i]):
+                    sample_number.append(i)
+                    test_image.append(x_test[i])
+                    predicted_label.append(y_test[i])
+                    actual_label.append(yhat[i])
+
         loss_list.append(loss.cpu().data)
         accuracy = correct / n_test
         accuracy_list.append(accuracy)
 
     return correct
 
-print("Num correct: ", train_model(15))
-print("len(validation_dataset)", len(validation_dataset))
+print("Num correct: ", train_model(1))
 torch.cuda.empty_cache()
-#print(loss_list)
-print(max(accuracy_list))
+print("Accuracy %", max(accuracy_list) * 100)
 
-fig, ax1 = plt.subplots()
+def show_misclassified_sample():
+    fig, ax1 = plt.subplots()
+    pred = predicted_label[0].cpu().numpy()
+    actual = predicted_label[0].cpu().numpy()
+    ax1.set_title(f"Predicted value: {pred}, Actual value: {actual}")
+    img = test_image[0].cpu().numpy()
+    plt.imshow(np.squeeze(img), cmap="gray")
+    plt.show()
 
-color = 'tab:red'
-ax1.plot(loss_list,color=color)
-ax1.set_xlabel('epoch',color=color)
-ax1.set_ylabel('total loss',color=color)
-ax1.tick_params(axis='y', color=color)
-   
-ax2 = ax1.twinx()  
-color = 'tab:blue'
-ax2.set_ylabel('accuracy', color=color)  
-ax2.plot( accuracy_list, color=color)
-ax2.tick_params(axis='y', color=color)
-fig.tight_layout()
-plt.show()
+def show_stats():
+    fig, ax1 = plt.subplots()
+    color = 'tab:red'
+    ax1.plot(loss_list,color=color)
+    ax1.set_xlabel('epoch',color=color)
+    ax1.set_ylabel('total loss',color=color)
+    ax1.tick_params(axis='y', color=color)
+    
+    ax2 = ax1.twinx()  
+    color = 'tab:blue'
+    ax2.set_ylabel('accuracy', color=color)  
+    ax2.plot( accuracy_list, color=color)
+    ax2.tick_params(axis='y', color=color)
+    fig.tight_layout()
+    plt.show()
+
+show_misclassified_sample()
